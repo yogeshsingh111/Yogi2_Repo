fbd10b4721b922677ec5c8d5d0f262c30df621bc
Yogesh Singh <eyogeshsingh111@gmail.com>
2025-07-11 20:25:55 +0530

    created a new repo

---
+++ main.tf
@@ -0,0 +1,82 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.35.0"
+    }
+  }
+}
+
+provider "azurerm" {
+    subscription_id = "92e22e38-2f32-450c-97de-3c896645b2da"
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  depends_on          = [azurerm_resource_group.rg]
+  name                = "${var.vm_name}-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  depends_on           = [azurerm_virtual_network.vnet]
+  name                 = "${var.vm_name}-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.0.0/24"]
+}
+
+resource "azurerm_public_ip" "pubip" {
+  name                = "${var.vm_name}-pubip"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+  sku                 = "Basic"
+}
+
+resource "azurerm_network_interface" "nic" {
+  depends_on          = [azurerm_subnet.subnet]
+  name                = "${var.vm_name}-nic"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pubip.id
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "winvm" {
+  name                  = var.vm_name
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = var.location
+  size                  = var.vm_size
+  admin_username        = var.admin_username
+  admin_password        = var.admin_password
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+    name                 = "${var.vm_name}-osdisk"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+
+  provision_vm_agent       = true
+  enable_automatic_updates = true
+}
